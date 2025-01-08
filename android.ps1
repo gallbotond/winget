249b20050e1f9c79b5f1ddd9444d58af74acca7d
botondg <botond.gall@sonrisa.hu>
2025-01-08 20:45:29 +0200

    update

---
+++ android.ps1
@@ -0,0 +1,5 @@
+winget install --id=Parsec.Parsec -e ;
+winget install --id=Google.AndroidStudio -e ;
+winget install --id=Oracle.JDK.17 -e ;
+winget install --id=Git.Git -e ;
+winget install --id=Notepad++.Notepad++ -e
